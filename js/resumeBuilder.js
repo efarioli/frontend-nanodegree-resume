fdb8286f7afe02482da042e2b382d51f8c8d5356
Ezequiel Farioli <efarioli@gmail.com>
2017-07-19 13:06:49 +0100

    set the structure of the data objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,112 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var bio = {
+    name: "Zeke Farioli",
+    role: "Front-end Web Developer",
+    contacts: {
+        mobile: "",
+        email: "",
+        github: "",
+        twitter: "",
+        location: ""
+    },
+    welcomeMessafe: "",
+    skills: [
+        "",
+        "",
+        ""
+    ],
+    display: function() {
+        console.log("displaying Bio");
+    }
+};
+
+var education = {
+    schools: [{
+            name: "",
+            location: "",
+            degree: "",
+            majors: "",
+            dates: "13-09-1979",
+            url: ""
+        },
+        {
+            name: "",
+            location: "",
+            degree: "",
+            majors: "",
+            dates: "13-09-1980",
+            url: ""
+        }
+    ],
+    onlineCourses: [{
+            title: "",
+            school: "",
+            dates: "----",
+            url: ""
+
+        },
+        {
+            title: "",
+            school: "",
+            dates: "----",
+            url: ""
+
+        }
+    ],
+    display: function() {
+        console.log("displaying online courses");
+    }
+
+};
+
+var work = {
+    jobs: [{
+            employer: "",
+            title: "",
+            location: "",
+            dates: "in progress"
+        },
+        {
+            employer: "",
+            title: "",
+            location: "",
+            dates: "in progress"
+        }
+
+    ],
+    display: function() {
+        console.log("displaying jobs..");
+    }
+
+
+};
+
+var projects = {
+    projects: [{
+            title: "",
+            dates: "",
+            description: "",
+            images: [
+                "url1",
+                "url2"
+            ]
+        },
+        {
+            title: "",
+            dates: "",
+            description: "",
+            images: [
+                "url1",
+                "url2"
+            ]
+        }
+
+    ],
+    display: function() {
+        console.log("dsiplaying projects");
+    }
+
+
+};
